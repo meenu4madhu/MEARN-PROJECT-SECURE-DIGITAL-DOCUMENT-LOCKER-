ec2f21f09e374c021d1685205ad51a9257ce3bc5
meenumadhu300@gmail.com <meenu4madhu>
2026-02-19 15:52:51 +0530

    sddlocker-frontend completed

--- src/pages/Pnf.jsx
+++ src/pages/Pnf.jsx
@@ -6,9 +6,9 @@ function Pnf() {
   const navigate=useNavigate()
   return (
     <>
-<div className='flex justify-center items-center flex-col my-10 text-white'> <img width={'500'} src="https://cdnl.iconscout.com/lottie/premium/thumb/404-error-page-animation-gif-download-3299959.gif" alt="pnf" /> 
+<div className='flex justify-center items-center flex-col my-10 text-white'> <img width={'500'} src="https://assets.dochipo.com/editor/animations/404-error/b6463d8b-ac87-42a7-ad59-6584a19a77a8.gif" alt="pnf" /> 
 <p className='text-xs'>Oh No !</p> <h1 className='text-2xl my-3 font-bold'>Look Like You're Lost</h1> 
-<p className='text-xs'>The page you are looking for is not available</p> <button  onClick={()=>navigate('/')} className='type:button my-5 text-white bg-blue-400 p-3 rounded'>Back Home</button> </div>
+<p className='text-xs'>The page you are looking for is not available</p> <button  onClick={()=>navigate('/')} className='type:button my-3 text-white bg-black p-3 rounded'>Back Home</button> </div>
     </>
   )
 }
